884a1c6ac1d46a58ead43efaaa8ca68ad35f2fd8
YevhDr <evgesha.drozdova@gmail.com>
2019-09-12 23:31:05 +0300

    nav opacity

--- src/components/HorizontalNavBar.vue
+++ src/components/HorizontalNavBar.vue
@@ -2,24 +2,27 @@
   <div>
 <div class="navigation">
         <div :style="{'margin-left': leftHeaderMargin, 'width': leftHeaderWidth }" id="navicons-container">
-        <div class="links">
+        <div class="links navicon" style="opacity:1" v-on:click="navMenuIconsOpacity">
                 <router-link
                   tag="img"
                   src="img/declarations.png"
-                  :to="{ name: 'horizontal-bar', params: { smth: 'hello_shit' } }"
+                  :to="{ name: 'horizontal-bar', params: { smth: 'hello_shit' } }"  
                 >Horizontal Bar</router-link>          
               </div>
-              <div class="links">
-                <router-link tag="img" src="img/text.png" :to="{ name: 'text-element' }">Text</router-link>
+              <div class="links navicon" @click="navMenuIconsOpacity">
+                <router-link 
+                tag="img" 
+                src="img/text.png" 
+                :to="{ name: 'text-element' }">Text</router-link>
              </div>
-              <div class="links">
+              <div class="links navicon" @click="navMenuIconsOpacity">
                 <router-link
                   tag="img"
                   src="img/doctors_table.png"
                   :to="{ name: 'bar-plots', params: { smth: 'hello_shit' } }"
                 >Bar Plots</router-link>
               </div>
-              <div class="links">
+              <div class="links navicon" @click="navMenuIconsOpacity">
                 <router-link
                   tag="img"
                   src="img/table.png"
@@ -27,7 +30,7 @@
                 >Doctors Table</router-link>
              </div>
 
-              <div class="links">
+              <div class="links navicon" @click="navMenuIconsOpacity">
                 <router-link
                   tag="img"
                   src="img/table.png"
@@ -35,7 +38,7 @@
                 >Med Table</router-link>
              </div>
 
-              <div class="links">
+              <div class="links navicon" @click="navMenuIconsOpacity">
                 <router-link
                   tag="img"
                   src="img/zakupivki.png"
@@ -86,6 +89,14 @@ export default {
          var width = headerBounding.width
          that.leftHeaderMargin = left + 33 + "px";
          that.leftHeaderWidth = width - 50 + "px";        
+    },
+    
+    navMenuIconsOpacity: function(event) {
+      var clickedElement = event.target;
+      d3.selectAll('.navicon img').style("opacity", 0.6)
+      var imgChild = clickedElement.querySelector('img');
+      d3.select(clickedElement).style("opacity", 1)
+      console.log(event.target)
     }
 }
 
