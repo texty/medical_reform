e43f6cfde60819a87e44cbaa9c0d66d23245cd5b
YevhDr <evgesha.drozdova@gmail.com>
2019-09-13 17:11:43 +0300

    fb and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+      <section id="created_texty">
+    <div>
+        <a href="https://www.facebook.com/sharer/sharer.php?u=http://http://texty.org.ua/d/2019/medical_reform/"
+           class="share-btn"><i class="fab fa-facebook-f"></i></a>
+        <a href="https://twitter.com/intent/tweet?text=http://http://texty.org.ua/d/2019/medical_reform/" class="share-btn"><i
+                class="fab fa-twitter"></i></a>
+        <!--<h3>Створено Текстами</h3>-->
+        <h3>Створено Текстами</h3>
+    </div>
+    <div>
+        <a href="http://texty.org.ua/" target="_blank"><img src="img/logo/black_logo.gif" alt="texty.org.ua"></a>
+    </div>
+</section>
+
     <div id="footer" :style="{  }">
         <div class="logoContainer">
             <img src="img/logo/Mindigital_white.png"/>
@@ -68,8 +82,8 @@ $blue: #184a77;
 
 #footer {
     background-color: white;
-    border-top: 2px solid $blue;
-    border-bottom: 2px solid $blue;
+    //border-top: 2px solid $blue;
+    //border-bottom: 2px solid $blue;
     height: 150px;
     width: 100%;
     margin:80px 0;
@@ -98,5 +112,52 @@ $blue: #184a77;
         max-height:100%;
      }
 }
+
+/* Created by Texty*/
+#created_texty {
+  letter-spacing: 1px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  justify-items: center;
+  align-content: center;
+  margin: 4em 15% 4em 15%;
+
+  i {
+    box-sizing: content-box;
+    margin: 0.5em;
+    cursor: pointer;
+    width: 1em;
+    height: 1em;
+    border: 1px solid grey;
+    color: lighten(grey, 15%);
+    border-radius: 50%;
+    padding: 0.5em;
+    font-size: 2.5em;
+    line-height: 1;
+    transition: all 0.5s;
+
+    &:hover {
+      border: 1px solid grey;
+      color: lighten(black, 15%);
+    }
+  }
+
+  img {
+    max-height: 4em;
+    max-width: 90%;
+  }
+
+  div {
+    text-align: center;
+  }
+
+  h3 {
+    text-align: center;
+    font-size: 1.5rem;
+  }
+}
+
 
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,6 +10,18 @@
       </div>
      </div>
 
+
+
+    <div id="logo-soc">
+        <div class="logo">
+<!--             <a href="http://texty.org.ua/" target="_blank"><img class='top-logo' style="width:100%" src="img/logо.jpg"/></a>
+ -->        </div>
+
+        <div id="soc">
+            <a id="facebook" href="https://www.facebook.com/sharer/sharer.php?u=http://http://texty.org.ua/d/2019/medical_reform/" target="_blank" class="share-btn"><i class="fab fa-facebook-square"></i></a>
+            <a id="twitter" href="https://twitter.com/intent/tweet?text=http://http://texty.org.ua/d/2019/medical_reform/" target="_blank" class="share-btn"><i class="fab fa-twitter-square"></i></a>
+        </div>
+    </div>
       
   </div>
 </template>
@@ -47,6 +59,83 @@ export default {
 </script>
 
 <style lang="scss">
+#logo-soc {
+  width:100%;
+  position:absolute;
+  top:0;
+  left:0;
+  display:flex;
+  justify-content: space-between;
 
+}
+
+.logo {
+  margin-left: 5px;
+  height:50px;
+  width:200px;
+  grid-column: 1/2;
+  @media screen and (min-width : 1824px){
+    width:180px;
+  }
+  @media screen and (min-width: 320px) and (max-width : 480px) {
+    width:120px;
+  }
+
+  a {
+    height: 100%;
+    display:flex;
+
+    img {
+      margin:auto;
+    }
+  }
+}
+
+#soc {
+  position:fixed;
+  top:5px;
+  right:0;
+  display:flex;
+  justify-content: flex-end;
+  margin-right:20px;
+
+  div#translate {
+    display:flex;
+  }
+
+  a {
+    margin:auto;
+    text-decoration: none;
+  }
+
+  a span {
+    border-bottom: 1px dashed black;
+    margin-right:20px;
+    font-weight:800;
+    font-size:20px;
+    letter-spacing: 1px;
+    color:black!important;
+    background-color:rgba(225,225,225,0.7);
+    @media screen and (min-width : 1824px){
+      font-size:14px;
+    }
+  }
+
+
+  i {
+    font-size:40px;
+    color: lightgray;
+    margin: 0 0 0 0.5em;
+    max-height: 1em;
+    transition: all 0.5s;
+    @media screen and (min-width: 320px) and (max-width : 480px) {
+      font-size:20px;
+    }
+
+    &:hover {
+      color: red;
+    }
+  }
+}
 
 </style>
