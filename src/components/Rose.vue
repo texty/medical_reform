76adcbc73329457ca8f61e9dfad33e24ea1dcef2
ptrBodnar <peterbodnar1213@gmail.com>
2019-10-27 23:34:24 +0200

    updates pharmacy page

--- src/components/Rose.vue
+++ src/components/Rose.vue
@@ -1,49 +1,98 @@
 <template>
   <div>
-    <div>gdgd gd dgd</div>
+    <div>{{hospital.division_name + " " + hospital.division_residence_addresses}}</div>
+
+    <!--       <p >
+      {{ hospital }}
+    </p>
+    -->
+<!--         <p>
+{{buffered}}
+    </p> -->
+   
     <svg :width="roseWidth" :height="roseHeight">
       <g>
-        <circle cx="60" cy="60" r="5" />
-        <circle :cx="project(31.941938, 50.000591 )[0]" :cy="project(31.941938, 50.000591)[1]" r="5" />
-        <circle :cx="project(32, 50)[0]" :cy="project(32, 50)[1]" r="5" />
+        <circle
+          :cx="project(hospital.nszu_geocoding_google_api_lng, hospital.nszu_geocoding_google_api_lat)[0]"
+          :cy="project(hospital.nszu_geocoding_google_api_lng, hospital.nszu_geocoding_google_api_lat)[1]"
+          r="10"
+          :class="hospital.nszu_geocoding_google_api_lng + ' ' + hospital.nszu_geocoding_google_api_lat"
+          fill="red"
+        />
+
+        <!--       <circle 
+        :cx="20"
+         :cy="20" 
+         r="10"
+         fill="red" />
+        -->
+
+        <!-- <circle
+          :cx="project(31.941938, 50.000591 )[0]"
+          :cy="project(31.941938, 50.000591)[1]"
+          r="5"
+        /> -->
+        <circle
+          v-for="(d, i) in hospital.aptekaObjects"
+          v-bind:key="i"
+          :cx="project(d.nszu_geocoding_google_api_lng, d.nszu_geocoding_google_api_lat)[0]"
+          :cy="project(d.nszu_geocoding_google_api_lng, d.nszu_geocoding_google_api_lat)[1]"
+          r="5"
+          fill="none"
+          stroke="black"
+          :class="d.nszu_geocoding_google_api_lng + ' ' + d.nszu_geocoding_google_api_lat"
+
+        />
+  <!--       <circle :cx="60" :cy="60" r="5" />
         <circle :cx="60" :cy="60" r="5" />
-        <circle :cx="60" :cy="60" r="5" />
-        <circle :cx="60" :cy="60" r="5" />
+        <circle :cx="60" :cy="60" r="5" /> -->
 
         <!-- 
             https://stackoverflow.com/questions/11808860/how-to-place-arrow-head-triangles-on-svg-lines
         how to add arrow at the end of path
-         -->
+        -->
       </g>
     </svg>
   </div>
 </template>
 
 <script>
-import * as turf from '@turf/turf'
+import * as turf from "@turf/turf";
 
 export default {
   props: {
     roseWidth: Number,
     roseHeight: Number,
-    hospitals: Array
+    hospital: Object
+  },
+  data() {
+    return {
+      buffered: null
+    };
   },
   computed: {
     projection() {
-    var point = turf.point([32,50]);
-    var buffered = turf.buffer(point, 8, {units:'kilometers'});
-    // debugger;
-    // var bbox = turf.bboxPolygon(buffered);  
+      var point = turf.point([
+        this.hospital.nszu_geocoding_google_api_lng,
+        this.hospital.nszu_geocoding_google_api_lat
+      ]);
+      var buffered = turf.buffer(point, 6, { units: "kilometers" });
+      // debugger;
+      // var bbox = turf.bboxPolygon(buffered);
 
-    // console.log(bbox)
+      // console.log(bbox)
+
+      this.buffered = buffered;
 
       var pr = d3
         .geoMercator()
-        // .center([50, 32])
-        // .translate([150, 150])
-        .fitExtent([[0, 0],
-         [this.roseWidth, this.roseHeight]], buffered)
-        // .scale(100000);
+        .center([
+          this.hospital.nszu_geocoding_google_api_lng,
+          this.hospital.nszu_geocoding_google_api_lat
+        ])
+        .translate([this.roseWidth / 2, this.roseHeight / 2])
+        //.fitSize([this.roseWidth, this.roseHeight], buffered)
+        .scale(70000);
 
       return pr;
     }
--- src/components/RoseChart.vue
+++ src/components/RoseChart.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <p>Here</p>
-    <Rose :roseWidth="200" :roseHeight="200" :hospitals="pmd" />
+    <Rose 
+    v-for="(d, i) in network"
+    v-bind:key="i"
+    :roseWidth="200" :roseHeight="200" :hospital="d" />
+
+<!--     <p 
+    v-for="(d, i) in network.slice(0,1)"
+    v-bind:key="i"
+    >
+      {{ d.aptekaObjects[0] }}
+    </p> -->
   </div>
 </template>
 
@@ -18,46 +28,64 @@ export default {
       pmd: null,
       apteka: null,
       network: null,
-    //   roseWidth: 100,
-    //   roseHeight: 100,
+      aptekyNested: null
+      //   roseWidth: 100,
+      //   roseHeight: 100,
     };
   },
+  computed: {},
   components: {
     Rose
   },
-  methods:{
-      project(lat, lng) {
+  methods: {
+    project(lat, lng) {
+      var x = (lng + 180) * (this.roseWidth / 360);
+      // convert from degrees to radians
+      var latRad = (lat * Math.PI) / 180;
+      // get y value
+      var mercN = Math.log(Math.tan(Math.PI / 4 + latRad / 2));
+      var y = this.roseHeight / 2 - (this.roseWidth * mercN) / (2 * Math.PI);
 
-        var x = (lng + 180) * (this.roseWidth / 360);
-        // convert from degrees to radians
-        var latRad = lat * Math.PI / 180;
-        // get y value
-        var mercN = Math.log(Math.tan((Math.PI / 4) + (latRad / 2)));
-        var y = (this.roseHeight / 2) - (this.roseWidth * mercN / (2 * Math.PI));
-
-        return x,y
-
-      }
+      return x, y;
+    }
   },
   created() {
     let that = this;
-    Promise.all([d3.csv("data/pmd_all_contracted_legal_entities.csv"), 
-    d3.csv("data/pharmacy_all_contracted_legal_entities.csv"),
-    d3.json("data/hospitals_and_pharmacies.json")])
+    Promise.all([
+      d3.csv("data/pmd_all_contracted_legal_entities.csv"),
+      d3.csv("data/pharmacy_all_contracted_legal_entities.csv"),
+      d3.json("data/hospitals_and_pharmacies.json")
+    ])
       .then(function(files) {
         // files[0] will contain file1.csv
         // files[1] will contain file2.csv
-        that.pmd = files[0]
-        that.apteka = files[1]
-        that.network = files[2]
+        that.pmd = files[0];
+        that.apteka = files[1];
+        /* that.network = files[2]; */
+
+        that.aptekyNested = d3
+          .nest()
+          .key(function(d) {
+            return d.division_id;
+          })
+          .map(that.apteka);
+
+        that.network = files[2].map(d => {
+          return {
+            ...d,
+            ...{
+              aptekaObjects: d.pharmas.map(dd => {
+                return that.aptekyNested.get(dd)[0];
+              })
+            }
+          };
+        }).slice(0,10);
+
+
       })
       .catch(function(err) {
         // handle error here
       });
-
-
-
-
   }
 };
 </script>
